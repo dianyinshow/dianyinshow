5fdd0d6147e5b61cbf13a2033c22b74fecf3fdcd
dianyinshow <glod69945968@qq.com>
2017-06-27 11:13:18 +0800

    ok

--- showWeb/public/assets/js/myNews.js
+++ showWeb/public/assets/js/myNews.js
@@ -1,79 +1,90 @@
-$(function(){
-	// 我的消息点击编辑出现
-	$('.edit-mynews').on('click',function(){
-		$('.am-main-header .editvd').show();
-		$('.myNews-right-edit .myNews-delall').show();
-		$(this).hide()
-	})
-	// 全选
-	$('.allcheck').on('click',function(){
-		// 点击全选
-		if($('.allcheck input').prop('checked')){
-			$(this).find('.discheck-state').addClass('checkstate');
-		}else{
-			$(this).find('.discheck-state').removeClass('checkstate');
-		}
-		// 单选状态
-		 $('.myNews-delall').find('input')
+$(function() {
+    // 我的消息点击编辑出现
+    $('.edit-mynews').on('click', function () {
+        $('.am-main-header .editvd').show();
+        $('.myNews-right-edit .myNews-delall').show();
+        $(this).hide()
+        $('.myNews-item').off('click');
+        $('.myNews-item').css({'background': '#fff'});
+        $('.myNews-item').find('.myNews-communicate').hide();
+        $('.myNews-item').find('.myNews-datas').show()
+
+    })
+    // 全选
+    $('.allcheck').on('click', function () {
+        // 点击全选
+        if ($('.allcheck input').prop('checked')) {
+            $(this).find('.discheck-state').addClass('checkstate');
+        } else {
+            $(this).find('.discheck-state').removeClass('checkstate');
+        }
+        // 单选状态
+        $('.myNews-delall').find('input')
             .prop('checked', $('.allcheck').find('input').prop('checked'));
-            change()
-	})
-	$('.myNews-delall input').on('change',function(){
-       // 点击单选列表
-            change()
-	})
-	// 点击删除列表内容
-	$('.edit-del').on('click',function(){
-		$('.myNews-delall input').each(function(){
-        //列表状态
-        var index=$(this).index()
-        if($('.myNews-delall input').eq(index).prop('checked')){
-            $('.myNews-delall input').eq(index).parents("li").remove();
-        }
-        if($('.myNews-delall input').length==0){
-        	$('.allcheck').find('input').prop('checked',false)
-            $('.allcheck').find('.discheck-state').removeClass('checkstate');
-        }
+        change()
+    })
+    $('.myNews-delall input').on('change', function () {
+        // 点击单选列表
+        change()
+    })
+    // 点击删除列表内容
+    $('.edit-del').on('click', function () {
+        $('.myNews-delall input').each(function () {
+            //列表状态
+            var index = $(this).index()
+            if ($('.myNews-delall input').eq(index).prop('checked')) {
+                $('.myNews-delall input').eq(index).parents("li").remove();
+            }
+            if ($('.myNews-delall input').length == 0) {
+                $('.allcheck').find('input').prop('checked', false)
+                $('.allcheck').find('.discheck-state').removeClass('checkstate');
+            }
         })
-	})
-	$('.edit-voer').on('click',function(){
-		$('.am-main-header .editvd').hide();
-		$('.myNews-right-edit .myNews-delall').hide();
-		$('.edit-mynews').show()
-	})
-	function change(){
-        var numa=0;//判断列表长度
-        $('.myNews-delall input').each(function(index){
-        //列表状态
-            if($('.myNews-delall input').eq(index).prop('checked')){
-                numa+=1
+    })
+    $('.edit-voer').on('click', function () {
+        $('.am-main-header .editvd').hide();
+        $('.myNews-right-edit .myNews-delall').hide();
+        $('.edit-mynews').show();
+        // $('.myNews-item').trigger('click');
+        itemcomment()
+    })
+    function change() {
+        var numa = 0;//判断列表长度
+        $('.myNews-delall input').each(function (index) {
+            //列表状态
+            if ($('.myNews-delall input').eq(index).prop('checked')) {
+                numa += 1
                 $(this).siblings("label").addClass('myNew-del-stateall')
-                $(this).parents('li').css({'background':'#fafafc'})
-            }else{
-            	 $(this).siblings("label").removeClass('myNew-del-stateall')
-            	 $(this).parents('li').css({'background':'#fff'})
+                $(this).parents('li').css({'background': '#fafafc'})
+            } else {
+                $(this).siblings("label").removeClass('myNew-del-stateall')
+                $(this).parents('li').css({'background': '#fff'})
             }
         })
         //全选状态
-        if(numa===$('.myNews-delall input').length){
-            $('.allcheck').find('input').prop('checked',true)
+        if (numa === $('.myNews-delall input').length) {
+            $('.allcheck').find('input').prop('checked', true)
             $('.allcheck').find('.discheck-state').addClass('checkstate');
 
-        }else{
-            $('.allcheck').find('input').prop('checked',false)
+        } else {
+            $('.allcheck').find('input').prop('checked', false)
             $('.allcheck').find('.discheck-state').removeClass('checkstate');
         }
-		  
-	}
-	// 继续沟通
-	$('.linkup-img').on('click',function(){
-		$('.myNews-item').css({'background':'#fff'});
-		$('.myNews-item').find('.myNews-communicate').hide();
-		$('.myNews-item').find('.myNews-datas').show()
-		$(this).parents('li').css({'background':'#fafafc'});
-		$(this).parents('li').find('.myNews-communicate').show();
-		$(this).parents('li').find('.myNews-datas').hide()
-	})
+
+    }
+
+    // 继续沟通
+    itemcomment()
+    function itemcomment() {
+    $('.myNews-item').on('click', function () {
+        $('.myNews-item').css({'background': '#fff'});
+        $('.myNews-item').find('.myNews-communicate').hide();
+        $('.myNews-item').find('.myNews-datas').show()
+        $(this).css({'background': '#fafafc'});
+        $(this).find('.myNews-communicate').show();
+        $(this).find('.myNews-datas').hide()
+    })
+}
 	// $('.myNews-item').on('mouseout',function(){
 	// 	$(this).css({'background':'#fff'});
 	// 	$(this).find('.myNews-communicate').hide();
